86d4e8f4281b1efe7d1d895727bf42e4f7497a84
SamriddhiB <110332382+SamriddhiB@users.noreply.github.com>
2022-07-31 15:51:24 +0530

    Update startOz.java

--- startOz.java
+++ startOz.java
@@ -1,14 +1,14 @@
-import java.util.Scanner;
+
 /*Given a string, return a string made of the first 2 chars (if present), however include first char only if it is 'o' and include the second only if it is 'z', so "ozymandias" yields "oz".
 startOz("ozymandias") → "oz"
 startOz("bzoo") → "z"
 startOz("oxx") → "o" */
-
+import java.util.Scanner;
 public class startOz {
   public static 
     public static void main(String[] args) {
 
-    // String str="ozyman";
+  
      Scanner sc=new Scanner(System.in);
      System.out.println("Enter your string!!!");
      String str=sc.next();
